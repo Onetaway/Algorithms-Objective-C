635c80824d5413dd034f5222250fdaf6043d4b16
Onetaway <onetaway@vip.qq.com>
2014-07-10 00:22:07 +0800

    finish add poly

--- Chapter 3/AddPolynomial.m
+++ Chapter 3/AddPolynomial.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Onet away. All rights reserved.
 //
 
+
 #import "AddPolynomial.h"
 
 @interface AddPolynomial ()
@@ -19,6 +20,8 @@
 - (AddPolynomial *)p_createPolynomiaTwo;
 + (void)p_printPolynomial:(AddPolynomial *)poly;
 - (AddPolynomial *)p_addPolyOne:(AddPolynomial *)polyOne polyTwo:(AddPolynomial *)polyTwo;
+- (AddPolynomial *)p_insertedNode:(AddPolynomial *)point;
+
 
 @end
 
@@ -26,12 +29,13 @@
 
 - (void)addPolynomial
 {
+    NSLog(@"多项式相加前:");
     AddPolynomial *polyOne = [self p_createPolynomiaOne];
     AddPolynomial *polyTwo = [self p_createPolynomiaTwo];
-    
     [AddPolynomial p_printPolynomial:polyOne];
     [AddPolynomial p_printPolynomial:polyTwo];
     
+    NSLog(@"多项式相加后:");
     AddPolynomial *polySum = [self p_addPolyOne:polyOne polyTwo:polyTwo];
     [AddPolynomial p_printPolynomial:polySum];
 }
@@ -88,27 +92,37 @@
     NSAssert(polyTwo, @"Polynomial Two can't be nil");
     
     AddPolynomial *resultHead = [[AddPolynomial alloc] init];
+    resultHead.coefficient = 0;
+    resultHead.exponent = 0;
+    resultHead.next = nil;
+    
     AddPolynomial *resultTail = resultHead;
     AddPolynomial *poly1 = polyOne;
     AddPolynomial *poly2 = polyTwo;
     
     while (poly1.next && poly2.next) {
         if (poly1.next.exponent < poly2.next.exponent) {
-            resultTail.next = poly2.next;
-            resultTail = resultTail.next;
+            
+            AddPolynomial *addNode = [self p_insertedNode:poly2.next];
+            
+            resultTail.next = addNode;
+            resultTail = addNode;
             poly2 = poly2.next;
             
         } else if (poly1.next.exponent > poly2.next.exponent) {
-            resultTail.next = poly1.next;
-            resultTail = resultTail.next;
+            
+            AddPolynomial *addNode = [self p_insertedNode:poly1.next];
+            
+            resultTail.next = addNode;
+            resultTail = addNode;
             poly1 = poly1.next;
             
         } else {
             
-            
             AddPolynomial *addNode = [[AddPolynomial alloc] init];
             addNode.coefficient = poly1.next.coefficient + poly2.next.coefficient;
             addNode.exponent = poly1.next.exponent;
+            addNode.next = nil;
             
             resultTail.next = addNode;
             poly1 = poly1.next;
@@ -119,6 +133,18 @@
     
     
     return resultHead;
+}
+
+- (AddPolynomial *)p_insertedNode:(AddPolynomial *)point
+{
+    NSAssert(point, @"point can't be nil");
+    
+    AddPolynomial *result = [[AddPolynomial alloc] init];
+    result.coefficient = point.coefficient;
+    result.exponent = point.exponent;
+    result.next = nil;
+    
+    return result;
 }
 
 @end
