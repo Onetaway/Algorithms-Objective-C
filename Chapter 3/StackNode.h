584113b59ec0656837802a1d1d3b3d68a5088df3
Onetaway <onetaway@vip.qq.com>
2014-07-16 22:37:30 +0800

    stack basic operation

--- Chapter 3/StackNode.h
+++ Chapter 3/StackNode.h
@@ -13,6 +13,56 @@
 @property (nonatomic, assign,readonly) int data;
 @property (nonatomic, strong, readonly) StackNode *next;
 
+/**
+ *  Check whether the stack is empty
+ *
+ *  @param stack The stack need to check
+ *
+ *  @return YES if the stack is empty, NO if the stack isn't empty
+ */
 + (BOOL)isEmpty:(StackNode *)stack;
 
+/**
+ *  Create a stack
+ *
+ *  @return The head node of the stack
+ */
+- (StackNode *)createStack;
++ (void)disposeStack:(StackNode *)stack;
++ (void)makeEmpty:(StackNode *)stack;
+
+/**
+ *  Push element to the stack.Note: The top is head node's next node.
+ *
+ *  @param element The element you want to insert
+ *  @param stack   The head node of the stack. It is also the top pointer.
+ */
++ (void)pushElement:(int)element inStack:(StackNode *)stack;
++ (int)topElement:(StackNode *)stack;
++ (void)popElement:(StackNode *)stack;
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Chapter 3/StackNode.m
+++ Chapter 3/StackNode.m
@@ -8,6 +8,106 @@
 
 #import "StackNode.h"
 
+@interface StackNode ()
+@property (nonatomic, assign,readwrite) int data;
+@property (nonatomic, strong, readwrite) StackNode *next;
+@end
+
 @implementation StackNode
 
++ (BOOL)isEmpty:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    
+    return stack.next == nil;
+}
+
+- (StackNode *)createStack
+{
+    StackNode *stack = [[StackNode alloc] init];
+    
+    stack.next = nil;
+    [StackNode makeEmpty:stack];
+    
+    return stack;
+}
+
++ (void)makeEmpty:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    
+    while (![StackNode isEmpty:stack]) {
+        [StackNode popElement:stack];
+    }
+}
+
++ (void)pushElement:(int)element inStack:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    
+    StackNode *dataNode = [[StackNode alloc] init];
+    dataNode.data = element;
+    dataNode.next = nil;
+    
+    dataNode.next = stack.next;
+    stack.next = dataNode;
+}
+
++ (int)topElement:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    NSAssert(stack.next, @"stack is empty");
+    
+    return stack.next.data;
+}
+
++ (void)popElement:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    NSAssert(stack, @"stack is empty");
+    
+    stack.next = stack.next.next;
+}
+
++ (void)disposeStack:(StackNode *)stack
+{
+    NSAssert(stack, @"stack can't be nil");
+    
+    stack.next = nil;
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
