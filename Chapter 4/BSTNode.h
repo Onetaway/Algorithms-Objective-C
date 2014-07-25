ef0624c825fff34cfc5bca3f082307f15e06d093
Onetaway <onetaway@vip.qq.com>
2014-07-25 11:49:57 +0800

    update BST

--- Chapter 4/BSTNode.h
+++ Chapter 4/BSTNode.h
@@ -6,7 +6,9 @@
 //  Copyright (c) 2014 Onet away. All rights reserved.
 //
 
-// Ex 4.10: 写出实现基本二叉查找树的例程
+// Ex 4.10: 写出实现基本二叉查找树的例程 (Binary Search Tree)
+
+// 概念：对于BST中的每一个结点x，x 的左子树的所有结点的值均小于x，而 x 的右子树的所有结点的值均大于x.
 
 #import <Foundation/Foundation.h>
 
@@ -16,6 +18,132 @@
 @property (nonatomic, strong) BSTNode *left;
 @property (nonatomic, strong) BSTNode *right;
 
+/**
+ *  make a BST tree empty
+ *
+ *  @param tree The BST tree you want to make empty
+ *
+ *  @return The pointer to the empty tree
+ */
++ (BSTNode *)makeEmpty:(BSTNode *)root;
+
+/**
+ *  Fine the element in the given BST tree
+ *
+ *  @param data The element you want to find
+ *  @param tree The BST tree you want to search
+ *
+ *  @return The pointer to the element node, if not found ,return nil.
+ */
++ (BSTNode *)findElement:(int)data inBSTTree:(BSTNode *)root;
+
+/**
+ *  Find the minium element in a BST tree (recursion version).
+ *
+ *  @param tree The BST tree you want to find
+ *
+ *  @return The pointer to the minium element.
+ */
++ (BSTNode *)findMinElementRecursion:(BSTNode *)root;
+
+/**
+ *  Find the minium element in a BST tree (non recursion version).
+ *
+ *  @param tree The BST tree you want to find
+ *
+ *  @return The pointer to the minium element.
+ */
++ (BSTNode *)findMinElementNonRecursion:(BSTNode *)root;
+
+/**
+ *  Find the max element in a BST tree (recursion version)
+ *
+ *  @param tree The BST tree you want to find
+ *
+ *  @return The pointer to the max element.
+ */
++ (BSTNode *)findMaxElementRecursion:(BSTNode *)root;
+
+/**
+ *  Find the max element in a BST tree (non recursion version)
+ *
+ *  @param tree The BST tree you want to find
+ *
+ *  @return The pointer to the max element.
+ */
++ (BSTNode *)findMaxElementNonRecursion:(BSTNode *)root;
+
+/**
+ *  Insert an element in a BST tree
+ *
+ *  @param data The element you want to insert
+ *  @param tree The BST tree you want to insert
+ *
+ *  @return The pointer to the root node of inserted BST tree.
+ */
++ (BSTNode *)insertElement:(int)data inBSTTree:(BSTNode *)root;
+
+/**
+ *  Delete an element in a BST tree.
+ *
+ *  @param data The element you want to delete.
+ *  @param tree The BST tree.
+ *
+ *  @return The pointer to the root node of deleted BST tree.
+ */
++ (BSTNode *)deleteElement:(int)data inBSTTree:(BSTNode *)root;
+
+/**
+ *  Get the node value in the given position in a given BST tree
+ *
+ *  @param position The position of the node which you want get the data value.
+ *  @param tree     The given BST tree.
+ *
+ *  @return The data value of the node.
+ */
++ (int)retrieveElementAtThePosition:(BSTNode *)position inBSTTree:(BSTNode *)root;
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Chapter 4/BSTNode.m
+++ Chapter 4/BSTNode.m
@@ -10,4 +10,142 @@
 
 @implementation BSTNode
 
++ (BSTNode *)makeEmpty:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    [BSTNode makeEmpty:root.left];
+    [BSTNode makeEmpty:root.right];
+    
+    return root;
+}
+
++ (BSTNode *)findElement:(int)data inBSTTree:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    if (data < root.data) {
+        return [BSTNode findElement:data inBSTTree:root.left];
+    } else if (data > root.data) {
+        return [BSTNode findElement:data inBSTTree:root.right];
+    } else {
+        return root;
+    }
+}
+
++ (BSTNode *)findMinElementRecursion:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    if (!root.left) {
+        return root;
+    } else {
+        return [BSTNode findMinElementRecursion:root.left];
+    }
+}
+
++ (BSTNode *)findMinElementNonRecursion:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    BSTNode *position = root;
+    
+    while (position.left) {
+        position = position.left;
+    }
+    
+    return position;
+}
+
++ (BSTNode *)findMaxElementRecursion:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    if (!root.right) {
+        return root;
+    } else {
+        return [BSTNode findMaxElementRecursion:root.right];
+    }
+}
+
++ (BSTNode *)findMaxElementNonRecursion:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    
+    BSTNode *position = root;
+    
+    while (position.right) {
+        position = position.right;
+    }
+    
+    return position;
+}
+
++ (BSTNode *)insertElement:(int)data inBSTTree:(BSTNode *)root
+{
+    if (!root) {
+        BSTNode *insertedNode = [[BSTNode alloc] init];
+        insertedNode.data = 0;
+        insertedNode.left = nil;
+        insertedNode.right = nil;
+        
+        root = insertedNode;
+        return root;
+    } else if (data < root) {
+        
+    }
+}
+
++ (BSTNode *)deleteElement:(int)data inBSTTree:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+}
+
++ (int)retrieveElementAtThePosition:(BSTNode *)position inBSTTree:(BSTNode *)root
+{
+    NSAssert(root, @"BST tree can't be nil");
+    NSAssert(position, @"position can't be nil");
+    
+    
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
